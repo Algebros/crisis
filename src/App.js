4342183abb8f504d916130fa271a40c99e72aead
Algebros <sofroniy.m@gmail.com>
2025-02-08 22:37:39 +0300

    implement HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter, BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Characters from './pages/Characters';
 import Roles from './pages/Roles';
@@ -8,7 +8,7 @@ import InteractiveMap from './pages/InteractiveMap'; // Import the new component
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="App">
         <nav>
           <ul>
@@ -33,7 +33,7 @@ function App() {
           <Route path="/map" element={<InteractiveMap />} /> {/* Add a new route for the map */}
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
