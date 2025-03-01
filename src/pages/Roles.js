8b02621c2129418d77d0cf4ae5dd5bde3ee63556
Algebros <sofroniy.m@gmail.com>
2025-03-01 11:00:18 +0300

    rework roles page

--- src/pages/Roles.js
+++ src/pages/Roles.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import data from '../assets/data.json';
 import data2 from '../assets/data_ru.json';
-import { Container, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { Container, Typography, Accordion, AccordionSummary, AccordionDetails, Grid2 } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useTranslation } from 'react-i18next';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
 
 function Roles() {
   const [factions, setFactions] = useState({});
@@ -20,38 +24,48 @@ function Roles() {
       return acc;
     }, {});
     setFactions(factionRoles);
-  }, [i18n.language]); // Re-run effect when language changes
+  }, [i18n.language]);
 
   return (
     <Container>
-      {/* <Typography variant="h1" gutterBottom>
-        Roles Page
-      </Typography> */}
       {Object.keys(factions).map(faction => (
         <div key={faction}>
           <Typography variant="h3" gutterBottom style={{ margin: '20px 0' }}>
             {faction}
           </Typography>
           <Typography variant="body1" gutterBottom>
-          {t(`factions.${faction}`)}
+            {t(`factions.${faction}`)}
           </Typography>
-          {factions[faction].map((role) => (
-            <Accordion key={role.name}>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h6">{role.name}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                  <Typography component="span" variant="body2" color="textPrimary">
-                    <span style={{ fontWeight: 'bold' }}>Description:</span> {role.description}
-                  </Typography>
-                  <Typography component="span" variant="body2" color="textPrimary">
-                    <span style={{ fontWeight: 'bold' }}>Feature:</span> {role.feature}
-                  </Typography>
-                </div>
-              </AccordionDetails>
-            </Accordion>
-          ))}
+
+          <Grid2 container direction="row" spacing={2}>
+            {factions[faction].map((role) => (
+              <Grid2>
+                <Card variant="outlined">
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="green iguana"
+                  />
+                </Card>
+              </Grid2>
+
+              // <Accordion key={role.name}>
+              //   <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              //     <Typography variant="h6">{role.name}</Typography>
+              //   </AccordionSummary>
+              //   <AccordionDetails>
+              //     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              //       <Typography component="span" variant="body2" color="textPrimary">
+              //         <span style={{ fontWeight: 'bold' }}>Description:</span> {role.description}
+              //       </Typography>
+              //       <Typography component="span" variant="body2" color="textPrimary">
+              //         <span style={{ fontWeight: 'bold' }}>Feature:</span> {role.feature}
+              //       </Typography>
+              //     </div>
+              //   </AccordionDetails>
+              // </Accordion>
+            ))}
+          </Grid2>
         </div>
       ))}
     </Container>
